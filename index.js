13e878e513b0ad286b22df272527b3486d2231e7
xena <74474948+a-andres1@users.noreply.github.com>
2021-02-27 10:06:18 -0500

    moved classes to correct files

--- index.js
+++ index.js
@@ -1,70 +1,9 @@
 const askyTime = require("inquirer"); 
 
 
-class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
 
-    getName(){
 
-    }
 
-    getId(){
 
-    }
-
-    getEmail(){
-
-    }
-
-    getRole(){
-
-    }
-
-}
-
-class Manager extends Employee {
-    constructor(officeNumber){
-        super(1);
-    }
-    
-    getRole() {
-
-        }
-    
-}
-
-class Engineer extends Employee {
-    constructor(gitHub){
-        super(gitHub);
-    }
-
-    getGithub(){
-
-    }
-    
-    getRole() {
-
-        }
-    
-}
-
-class Intern extends Employee {
-    constructor(school){
-        super(school);
-    }
-
-    getSchool(){
-        
-    }
-    
-    getRole() {
-
-        }
-    
-}
 
 
