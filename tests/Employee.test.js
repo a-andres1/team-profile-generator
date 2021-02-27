fbe1bc94a8edb8226d2ab4079fff900355cefe64
xena <74474948+a-andres1@users.noreply.github.com>
2021-02-27 10:07:06 -0500

    started wriing tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,7 +4,7 @@ describe("Employee", () => {
     describe("name", () => {
         it("should return an employee's name as a string", () => {
 
-            if
+            
             
         })
     })
