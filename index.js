476aca54f3585c916fba9162e5180cd9af39b894
xena <74474948+a-andres1@users.noreply.github.com>
2021-02-26 22:38:02 -0500

    added Employee class

---
+++ index.js
@@ -0,0 +1,31 @@
+const askyTime = require("inquirer"); 
+
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+
+    }
+
+    getId(){
+
+    }
+
+    getEmail(){
+
+    }
+
+    getRole(){
+
+    }
+
+}
+
+
+
+
