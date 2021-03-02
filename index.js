b540f30dfc31c1a694489adad08e5a1494bfda03
alyssa andres <alyssaandres1@gmail.com>
2021-03-01 22:21:38 -0500

    functions, for inquirer prompts

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const askyTime = require("inquirer"); 
 
+function startingPrompt(){
+    inquirer.prompt([
 
+
+    ])
+
+}
+
+function employee(){
+    inquirer.prompt([
+
+
+    ])
+}
+
+function manager(){
+    inquirer.prompt([
+
+    ])
+
+}
+
+function intern(){
+    inquirer.prompt([
+
+    ])
+}
+
+
+function engineer(){
+    inquirer.prompt([
+        
+    ])
+
+}
 
 
 
